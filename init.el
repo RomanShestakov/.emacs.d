60a8fc290fd64461c4df3042dbfcad9fc52f4f2c
ROMAN SHESTAKOV <romanshestakov@imac.home>
2014-04-06 14:50:43 +0100

    small formatting fixes

--- init.el
+++ init.el
@@ -1,29 +1,37 @@
-;; Common Lisp
-(require 'cl)
+;;; init.el --- entry point for configuration
+
+;;; Commentary:
+;; the root tree of calls to various modes
+
+;;; Code:
+
+(with-no-warnings
+  (require 'cl))
 
 ;; root of all emacs-related stuff
 (defvar emacs-root (if (or (eq system-type 'cygwin)
                            (eq system-type 'gnu/linux)
                            (eq system-type 'linux)
                            (eq system-type 'darwin))
-                       "~/.emacs.d/"    "z:/.emacs.d/"))
+                       "~/.emacs.d/"    "z:/.emacs.d/")
+  "*Path to EMACS root.")
 
 ;; path to where plugins are kept
-(setq plugin-path (concat emacs-root "el-get"))
+(defvar plugin-path (concat emacs-root "el-get")
+  "*Path to el-get plugins.")
 
 ;; add paths to various configuration modes
-(cl-labels ((add-path (p)
-                   (add-to-list 'load-path (concat emacs-root p))))
+(cl-labels ((add-path (p) (add-to-list 'load-path (concat emacs-root p))))
   (add-path  ".")
   (add-path  "settings")
   (add-path  "site-lisp")
-  (add-path  "scala") ;; scala mode related code
   (add-path  "erlang") ;; Configuration for Erlang mode
-  (add-path  "exec-path-from-shell") ;; allows setting PATH, PYTHONPATH from .profile 
+  (add-path  "exec-path-from-shell") ;; allows setting PATH, PYTHONPATH from .profile
+  ;; (add-path  "scala") ;; scala mode related code
   )
 
 ;; load a path to elang list as distel install in el-get distel package depends on it
-(setq load-path  (cons "/usr/local/lib/erlang/lib/tools-2.6.13/emacs" load-path))
+(add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.6.13/emacs")
 
 ;; now load various configs
 ;; set PATH, because we don't load .bashrc
@@ -34,8 +42,8 @@
 ;; some custom helper funcs
 (require 'custom-functions)
 
-;; load plugins with el-get 
-(require 'el-get-settings) 
+;; load plugins with el-get
+(require 'el-get-settings)
 
 ;; apply general customisation settings
 (require 'general-settings)
@@ -44,7 +52,7 @@
 (include-plugin "tramp")
 (require 'tramp)
 
-;; git 
+;; git
 (include-plugin "magit")
 (require 'magit)
 
@@ -57,10 +65,9 @@
 ;; add settings for scala mode
 ;; (require 'scala_mode_config)
 ;; add settings for erlang mode
-(require 'erlang-settings) 
+(require 'erlang-settings)
 ;; add settings for python mode
-(require 'python-settings) 
-;;(require 'flymake)
+(require 'python-settings)
 (require 'drag-and-drop-settings)
 (require 'key-binding-settings)
 (require 'fill-column-indicator-settings)
@@ -71,3 +78,5 @@
 (require 'dirtree-settings)
 (require 'yasnippet-settings)
 (require 'flycheck-mode-settings)
+
+;;; init.el ends here
