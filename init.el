e568f3b0d4afcfac26a6e16a6642688582f0cc99
ROMAN SHESTAKOV <romanshestakov@imac.home>
2014-04-09 21:27:40 +0100

    clean up

--- init.el
+++ init.el
@@ -51,7 +51,6 @@
 (move-text-default-bindings)
 
 ;; add modes with customized settings
-;; (require 'scala_mode_config)
 (require 'erlang-settings)
 (require 'python-settings)
 (require 'drag-and-drop-settings)
@@ -65,5 +64,18 @@
 (require 'yasnippet-settings)
 (require 'flycheck-mode-settings)
 (require 'org-mode-settings)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((checkdoc-minor-mode . t) (require-final-newline . t) (mangle-whitespace . t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;;; init.el ends here
