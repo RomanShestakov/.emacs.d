8ccd5fba0e320ecb06ab9664af9bd08bd77afa9c
ROMAN SHESTAKOV <romanshestakov@imac.home>
2014-04-27 10:48:26 +0100

    don't hardcode the path to erlang tools

--- settings/erlang-path-settings.el
+++ settings/erlang-path-settings.el
@@ -19,8 +19,10 @@
 (require 'exec-path-from-shell)
 (exec-path-from-shell-copy-env "ERL_TOP")
 
-;; set path to erlang install
-;; load a path to elang lisp as distel install in el-get distel package depends on it
+;; set path to erlang install load a path to elang lisp as distel
+;; install in el-get distel package depends on it if root is nil or
+;; empty (because env ERL_TOP is not setup, default to standard erlang
+;; location "/usr/local/lib/erlang"
 (defvar erlang-root
   (let ((root (getenv "ERL_TOP")))
     (if (and (not root)
@@ -30,17 +32,30 @@
   "*Path to Erlang installation.
 Env var ERL_TOP needs to be set in bash environment.  If ERL_TOP is not set, return  /usr/local/lib/erlang.")
 
+;; add lib to erlang-root
 (defvar erlang-root-dir
-  (concat erlang-root "/lib")
+  (concat (file-name-as-directory erlang-root) "lib")
   "*Path to erlang lib.")
-(setq exec-path (cons (concat erlang-root "/bin") exec-path))
-(add-to-list 'load-path (concat erlang-root "/lib/tools-2.6.14/emacs"))
 
+;; add erlang bin dir to exec-path
+(setq exec-path (cons (concat (file-name-as-directory erlang-root) "bin") exec-path))
+
+;; find the name of tools- directory
+(defun get-erlang-tools-dir-name ()
+  "*Get the name of tools dir in current erlang installation."
+  (car (let ((default-directory erlang-root-dir))
+         (file-expand-wildcards "tools-*"))))
+
+;; build the full absolute name to erlang tools
+(defun get-full-path-to-erlang-tools-dir ()
+  "*Get full name of erlang tools dir."
+  (concat (file-name-as-directory
+           (concat (file-name-as-directory erlang-root-dir) (get-erlang-tools-dir-name)))
+          "emacs"))
+
+;; add erlang tools to load-path
+(add-to-list 'load-path (get-full-path-to-erlang-tools-dir))
 
 (provide 'erlang-path-settings)
-(require 'erlang-path-settings)
 
 ;;; erlang-path-settings.el ends here
-
-
-
