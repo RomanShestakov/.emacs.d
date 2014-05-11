562871000e8c20b7fda6a2da3d4202df4aa32d27
ROMAN SHESTAKOV <romanshestakov@imac.home>
2014-05-11 21:44:20 +0100

    add .emacs-custom.el to keep custom settings added by custom

---
+++ .emacs-custom.el
@@ -0,0 +1,15 @@
+;;; .emacs-custom.el --- entries added by custom mode
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((checkdoc-minor-mode . t) (require-final-newline . t) (mangle-whitespace . t)))))
+
+;;; .emacs-custom.el ends here
--- init.el
+++ init.el
@@ -83,11 +83,7 @@
 (require 'elisp-slime-nav-settings)
 ;;(require 'fill-column-indicator-settings)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((checkdoc-minor-mode . t) (require-final-newline . t) (mangle-whitespace . t)))))
+(setq custom-file (concat (file-name-as-directory emacs-root) ".emacs-custom.el"))
+(load custom-file 'noerror)
 
 ;;; init.el ends here
