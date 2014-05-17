4a23ae771fd748d3d3d6713949f7121232a65054
ROMAN SHESTAKOV <romanshestakov@imac.home>
2014-05-17 21:27:32 +0100

    some refactoring

--- settings/color-theme-settings.el
+++ settings/color-theme-settings.el
@@ -3,16 +3,21 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'custom-functions)
+(autoload 'system-is-mac "custom-functions" t)
+
+(defvar emacs-root)
 
 ;; load paths to favorite themes
-(add-to-list 'custom-theme-load-path (concat (file-name-as-directory emacs-root) "gruber-darker-theme"))
+(add-to-list 'custom-theme-load-path
+             (concat (file-name-as-directory emacs-root) "gruber-darker-theme"))
 
 ;; load gruber-darker
 (if (>= emacs-major-version 23)
     (load-theme 'gruber-darker t)
-  (require 'color-theme-gruber-darker)
-  (color-theme-gruber-darker))
+  (load "color-theme-gruber-darker")
+  (eval-after-load "color-theme-gruber-darker"
+    '(progn
+       (color-theme-gruber-darker))))
 
 ;; set up fonts
 ;; http://superuser.com/questions/210555/emacs-font-settings-not-working-in-new-frame
@@ -26,12 +31,12 @@
 ;;(set-default-font "Inconselata-13")
 ;;(set-default-font "Monaco-13")
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
 
 (provide 'color-theme-settings)
 
