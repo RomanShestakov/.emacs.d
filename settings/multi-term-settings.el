31468fafc44ac23a575c192df47621d733c76f5c
ROMAN SHESTAKOV <romanshestakov@iMac.home>
2014-11-17 22:13:36 +0000

    add shortcuts for multiterm

--- settings/multi-term-settings.el
+++ settings/multi-term-settings.el
@@ -15,6 +15,9 @@
      (setq multi-term-program "/bin/bash")
      ))
 
+(global-set-key (kbd "C-c t") 'multi-term-next)
+(global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
+
 (provide 'multi-term-settings)
 
 ;;; multi-term-settings.el ends here
