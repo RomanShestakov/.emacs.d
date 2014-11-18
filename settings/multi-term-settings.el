fde718e39351b25c418cfb9203bdc2ae93c1255e
ROMAN SHESTAKOV <romanshestakov@iMac.home>
2014-11-18 20:37:56 +0000

    disable yasnipper for multi-term

--- settings/multi-term-settings.el
+++ settings/multi-term-settings.el
@@ -8,15 +8,31 @@
 ;;; keys shortcuts
 ;; M-r - (same as Ctr-r) search history.
 
+;; some interesting customisation for multi-term
+;; https://github.com/daimrod/Emacs-config/blob/master/config/config-multi-term.el
+
 ;; setup shell program to be used with multi-term
 
 (eval-after-load "multi-term"
   '(progn
-     (setq multi-term-program "/bin/bash")
+     (setq multi-term-program "/bin/bash"
+           term-unbind-key-list '("C-x"
+                                  "C-h"
+                                  "M-x"
+                                  "C-z")
+           term-term-name "xterm-256color" )
+
+     ;; create a new term or switch to existing one
+     (global-set-key (kbd "C-c t") 'multi-term-next)
+
+     ;; create a new one unconditionally
+     (global-set-key (kbd "C-c T") 'multi-term)
+
+     ;; ;; disable yasnippet
+     (add-hook 'term-mode-hook (lambda()
+                                 (yas-minor-mode -1)))
      ))
 
-(global-set-key (kbd "C-c t") 'multi-term-next)
-(global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
 
 (provide 'multi-term-settings)
 
