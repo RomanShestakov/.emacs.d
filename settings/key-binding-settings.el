1e674d160f1b07be18cc1576145f598b115c4e9f
RomanShestakov <romanshestakov@yahoo.co.uk>
2015-05-16 21:35:58 +0100

    add keybinding to make some keybindings work with iTerm2

--- settings/key-binding-settings.el
+++ settings/key-binding-settings.el
@@ -15,6 +15,17 @@
 (global-set-key (kbd "M-S-<left>") 'move-border-left)
 (global-set-key (kbd "M-S-<right>") 'move-border-right)
 
+;; http://stackoverflow.com/questions/26171265/emacs-keyboard-bindings-on-os-x-iterm2
+;; hardcode keybinding to make emacs work with iTerm2
+(define-key input-decode-map "\e[1;10A" [M-S-up])
+(define-key input-decode-map "\e[1;10B" [M-S-down])
+(define-key input-decode-map "\e[1;10C" [M-S-right])
+(define-key input-decode-map "\e[1;10D" [M-S-left])
+(define-key input-decode-map "\e[1;9A" [M-up])
+(define-key input-decode-map "\e[1;9B" [M-down])
+(define-key input-decode-map "\e[1;9C" [M-right])
+(define-key input-decode-map "\e[1;9D" [M-left])
+
 ;; binding to resize window : http://www.emacswiki.org/emacs/WindowResize
 ;; (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 ;; (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
