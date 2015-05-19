68efd3db3ab3d4e2f7cc75ae9a355481ca913f3d
RomanShestakov <romanshestakov@yahoo.co.uk>
2015-05-19 22:06:45 +0100

    remove erlang-path-settings

--- settings/erlang-settings.el
+++ settings/erlang-settings.el
@@ -12,8 +12,47 @@
   `(let ((,var ,value))
      (when ,var ,@body)))
 
+;; set path to erlang install load a path to elang lisp as distel
+;; install in el-get distel package depends on it if root is nil or
+;; empty (because env ERL_TOP is not setup, default to standard erlang
+;; location "/usr/local/lib/erlang"
+(defvar erlang-root
+  (let ((root (getenv "ERL_TOP")))
+    (if (and (not root)
+             (not (equal "" root)))
+        root
+      "/usr/local/lib/erlang"))
+  "*Path to Erlang installation.
+Env var ERL_TOP needs to be set in bash environment.  If ERL_TOP is not set, return  /usr/local/lib/erlang.")
+
+;; add lib to erlang-root
+(defvar erlang-root-dir
+  (concat (file-name-as-directory erlang-root) "lib")
+  "*Path to erlang lib.")
+
+;; add erlang bin dir to exec-path
+(setq exec-path (cons (concat (file-name-as-directory erlang-root) "bin") exec-path))
+
+;; find the name of tools- directory
+(defun get-erlang-tools-dir-name ()
+  "*Get the name of tools dir in current erlang installation."
+  (car (let ((default-directory erlang-root-dir))
+         (file-expand-wildcards "tools-*"))))
+
+;; build the full absolute name to erlang tools
+(defun get-full-path-to-erlang-tools-dir ()
+  "*Get full name of erlang tools dir."
+  (concat (file-name-as-directory
+           (concat (file-name-as-directory erlang-root-dir) (get-erlang-tools-dir-name)))
+          "emacs"))
+
+;;;###autoload
+(defun erlang-path-init()
+  "*Sets the paths to erlang mode."
+  (add-to-list 'load-path (get-full-path-to-erlang-tools-dir)))
+
 ;; set paths to erlang libs
-(autoload 'erlang-path-init "erlang-path-settings" nil)
+;;(autoload 'erlang-path-init "erlang-path-settings" nil)
 (erlang-path-init)
 (load "erlang-start" 'no-error)
 
