d4b1cfaaf33b49807dac40b26e025043bfe54939
RomanShestakov <romanshestakov@yahoo.co.uk>
2015-06-27 20:50:43 +0100

    add funcs to copy/cut without selecting lines first

--- init.el
+++ init.el
@@ -238,6 +238,24 @@
 ;; bind C-x r to rgrep command
 (global-set-key (kbd "C-x r") 'rgrep)
 
+;; copy / cut lines without selecting them first
+;; http://emacs-fu.blogspot.de/2009/11/copying-lines-without-selecting-them.html
+;; if there is not selection, assume the operation should be applied to the whole line.
+;; binded to M-w
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+    (if mark-active (list (region-beginning) (region-end))
+      (list (line-beginning-position)
+        (line-beginning-position 2)))))
+
 ;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
