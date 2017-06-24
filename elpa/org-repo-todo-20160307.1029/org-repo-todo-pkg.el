b0e79b72d47f19a6db4a4bb574313a9d8bc3724a
ROMAN SHESTAKOV <romanshestakov@iMac.home>
2017-06-24 12:55:20 +0100

    remove elpa

--- elpa/org-repo-todo-20160307.1029/org-repo-todo-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-repo-todo" "20160307.1029" "Simple repository todo management with org-mode" 'nil :commit "b164bacefcd3c55dd40cd1a9e91ffefd315b400d" :url "https://github.com/waymondo/org-repo-todo" :keywords '("convenience"))
