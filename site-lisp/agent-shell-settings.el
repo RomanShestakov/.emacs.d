b18a21e3a965be09d54865da24afe12fbba1dac5
RomanShestakov <RomanShestakov@yahoo.co.uk>
2026-04-18 13:43:04 +0100

    add agent shell

--- site-lisp/agent-shell-settings.el
+++ site-lisp/agent-shell-settings.el
@@ -4,6 +4,17 @@
 ;;; https://github.com/xenodium/agent-shell
 ;;; external deps: npm install -g @agentclientprotocol/claude-agent-acp
 
+;; # Install nvm
+;; curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+;; # Reload shell
+;; source ~/.bashrc
+
+;; # Install and use Node 20
+;; nvm install 20
+;; nvm use 20
+;; nvm alias default 20  # make it permanent
+
 ;;; Code:
 
 (use-package agent-shell
